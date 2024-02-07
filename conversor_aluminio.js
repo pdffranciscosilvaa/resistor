60eea25cdd19f53ea317422fdd2e3cb76f72fca9
Francisco <francisco__ls_lima@estudante.sesisenai.org.br>
2024-02-07 14:54:50 -0300

    conversor para o alumínio

---
+++ conversor_aluminio.js
@@ -0,0 +1,42 @@
+
+let resistividade = 0.0282
+
+let comprimento = 1555
+let areaTransversal = 2.5  //o valor é 1 para não dividir por zero
+let resistencia = 0
+
+resistencia = (resistividade * comprimento) / areaTransversal
+console.log("O valor da resistência é = ", resistencia)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
